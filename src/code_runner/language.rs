77cd7e2da411cb1a4c1a4f6beaf697b833bd2c35
Petter Rasmussen <petter@hask.no>
2020-12-01 00:06:43 +0100

    Use elm make instead of elm-make

--- src/code_runner/language.rs
+++ src/code_runner/language.rs
@@ -159,7 +159,7 @@ pub fn run_instructions(language: &Language, files: non_empty_vec::NonEmptyVec<p
         Language::Elm => {
             RunInstructions{
                 build_commands: vec![
-                    format!("elm-make --output a.js {}", main_file_str)
+                    format!("elm make --output a.js {}", main_file_str)
                 ],
                 run_command: "elm-runner a.js".to_string()
             }
